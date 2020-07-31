c1dfeb261417b1124baf2793e05abe73cdd73001
chavaliInfy <60637623+chavaliInfy@users.noreply.github.com>
2020-07-31 20:13:08 +0530

    Update SpringBootMariaDBApplicationTests.java

--- src/test/java/guru/springframework/SpringBootMariaDBApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootMariaDBApplicationTests.java
@@ -6,10 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-# @SpringBootTest
+ # @SpringBootTest
 public class SpringBootMariaDBApplicationTests {
 
-	@Test
+#	@Test
 	public void contextLoads() {
 	}
 
